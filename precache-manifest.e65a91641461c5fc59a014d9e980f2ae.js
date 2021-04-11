f04ae419016158d257be72afaa57a5820b307989
Bharat <kaushik.bharat3546@gmail.com>
2021-04-11 18:48:36 +0530

    deploy

---
+++ precache-manifest.e65a91641461c5fc59a014d9e980f2ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6557c8fba06d2fdeb4fd",
+    "url": "css/app.673114d1.css"
+  },
+  {
+    "revision": "28baa1e9ea3a228797cf",
+    "url": "css/chunk-vendors.fb0a0c31.css"
+  },
+  {
+    "revision": "54fe0779fafe4f6a8d59991837062e3b",
+    "url": "index.html"
+  },
+  {
+    "revision": "6557c8fba06d2fdeb4fd",
+    "url": "js/app.87d3ecf1.js"
+  },
+  {
+    "revision": "28baa1e9ea3a228797cf",
+    "url": "js/chunk-vendors.2610dfcc.js"
+  },
+  {
+    "revision": "3b041e81e8f05d36e74aebd44ab56da3",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
